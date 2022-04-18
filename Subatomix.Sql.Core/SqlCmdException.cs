a85aa463dc0333a282ba8b7f435f39df0ce6ea20
Jeff Sharp <public@sharpjs.net>
2022-04-17 19:32:14 -0700

    Document.

--- Subatomix.Sql.Core/SqlCmdException.cs
+++ Subatomix.Sql.Core/SqlCmdException.cs
@@ -18,18 +18,54 @@ using System.Runtime.Serialization;
 
 namespace Subatomix.Sql.Core;
 
+/// <summary>
+///   Represents an error that occurred during the operation of
+///   <see cref="SqlCmdPreprocessor"/>.
+/// </summary>
 [Serializable]
 public class SqlCmdException : Exception
 {
+    /// <summary>
+    ///   Initializes a new <see cref="SqlCmdException"/> instance with a
+    ///   default message.
+    /// </summary>
     public SqlCmdException()
-        : base ("An error occurred during SqlCmd preprocessing.") { }
+        : base("An error occurred during SqlCmd preprocessing.") { }
 
+    /// <summary>
+    ///   Initializes a new <see cref="SqlCmdException"/> instance with the
+    ///   specified message.
+    /// </summary>
+    /// <param name="message">
+    ///   A message that describes the exception.
+    /// </param>
     public SqlCmdException(string message)
         : base(message) { }
 
-    public SqlCmdException(string message, Exception innerException)
+    /// <summary>
+    ///   Initializes a new <see cref="SqlCmdException"/> instance with the
+    ///   specified message and inner exception.
+    /// </summary>
+    /// <param name="message">
+    ///   A message that describes the exception.
+    /// </param>
+    /// <param name="innerException">
+    ///   The inner exception that is the cause of the new exception, or
+    ///   <see langword="null"/>.
+    /// </param>
+    public SqlCmdException(string message, Exception? innerException)
         : base(message, innerException) { }
 
+    /// <summary>
+    ///   Initializes a new <see cref="SqlCmdException"/> instance with
+    ///   serialized data.
+    /// </summary>
+    /// <param name="info">
+    ///   The serialized object data from which to construct the exception.
+    /// </param>
+    /// <param name="context">
+    ///   Contextual information about the deserialization operation.
+    /// </param>
     protected SqlCmdException(SerializationInfo info, StreamingContext context)
         : base(info, context) { }
 }
