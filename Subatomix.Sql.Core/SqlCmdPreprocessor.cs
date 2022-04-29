afe94e3430f5c7e049a732d7aa32e3881bd53dbc
Jeff Sharp <public@sharpjs.net>
2022-04-28 17:51:11 -0700

    Extract GetNextPowerOf2Saturating to separate file.

--- Subatomix.Sql.Core/SqlCmdPreprocessor.cs
+++ Subatomix.Sql.Core/SqlCmdPreprocessor.cs
@@ -19,6 +19,7 @@ using System.Text.RegularExpressions;
 
 namespace Subatomix.Sql.Core;
 
+using static MathHelpers;
 using static RegexOptions;
 
 /// <summary>
@@ -391,23 +392,6 @@ public class SqlCmdPreprocessor
         }
 
         return builder;
-    }
-
-    private static int GetNextPowerOf2Saturating(int value)
-    {
-        // https://graphics.stanford.edu/~seander/bithacks.html#RoundUpPowerOf2
-        // but saturating at int.MaxValue instead of overflow
-
-        value--;
-        value |= value >>  1;
-        value |= value >>  2;
-        value |= value >>  4;
-        value |= value >>  8;
-        value |= value >> 16;
-
-        return value == int.MaxValue
-            ? value         // edge case: avoid overflow
-            : value + 1;    // normal
     }
 
     private class Input
